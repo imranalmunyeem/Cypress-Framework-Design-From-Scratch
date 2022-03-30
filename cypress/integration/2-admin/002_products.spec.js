e13834a0cd57a85f128abdfaaa468fb1219702e3
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-03-30 21:47:10 +0600

    m products

--- cypress/integration/2-admin/002_products.spec.js
+++ cypress/integration/2-admin/002_products.spec.js
@@ -2,25 +2,21 @@
 
 import ProductsPO from "../../support/pageobjects/ProductsPO";
 
-describe('Products',()=>{
+describe('Products Page',()=>{
 
     const productspo = new ProductsPO();
 
     beforeEach('Will run before each it block',()=>{
         cy.adminLogin('admin@yourstore.com', 'admin');
-    })
+    });
 
-    
-    it('Adding a new product should be successful with valid values',()=>{
-        cy.visit(Cypress.env('createProductUrl'))
-        productspo.inputProductName('Test Product');
-        productspo.inputShortDescription('Test short description');
-       // productspo.inputFullDescription('Test full description');
-        productspo.inputSku('Test SKU')
-        //productspo.selectCategory('14','Books');
-        cy.get('#autocomplete').type('ind')
-
-
-    })
-    
-})
+    context('Add Product Verification',()=>{
+        it('Adding a new product should be successful with valid values',()=>{
+            cy.visit(Cypress.env('createProductUrl'));
+            productspo.inputProductName('Test Product');
+            productspo.inputShortDescription('Test short description');
+           // productspo.inputFullDescription('Test full description');
+            productspo.inputSku('Test SKU')
+        });
+    });  
+});
